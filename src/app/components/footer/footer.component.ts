940864ebe719d1464ba704c56b989f0f81158bd8
Manuel Aguilera <102045443+aguileraDev@users.noreply.github.com>
2025-01-20 16:18:05 -0400

    style: add menu footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuFooterComponent } from "../menu-footer/menu-footer.component";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MenuFooterComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
